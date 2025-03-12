e99d07dc70e623735af162b9082fe07cc5181aa0
Ryan Vemulapalli <ryanvemulapalli@gmail.com>
2025-03-12 19:08:01 -0400

    Added the burger menu and created the js to make it functional.

--- src/javascript/header.js
+++ src/javascript/header.js
@@ -4,4 +4,54 @@ function insertHeader() {
     .then(data => document.getElementById('header').innerHTML = data);
 }
 
-insertHeader()
+let currentBurgerState = false;
+
+function toggleBurgerMenu(){
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 975) {
+      currentBurgerState = false;
+      document.querySelector(".js-burger-menu").innerHTML = `
+      <div class="burger-menu">
+        <ul class="burger-links" >
+          <li><a href="index.html">Projects</a></li>
+          <li><a href="index.html">About</a></li>
+          <li><a href="index.html">Blog</a></li>
+          <li><a href="index.html">Contact</a></li>
+        </ul>
+      </div>`
+    }
+  });
+
+  if (currentBurgerState === false){
+    currentBurgerState = true;
+    document.querySelector(".js-burger-menu").innerHTML = `
+      <div class="burger-menu active">
+          <ul class="burger-links" >
+            <li><a href="index.html">Projects</a></li>
+            <li><a href="index.html">About</a></li>
+            <li><a href="index.html">Blog</a></li>
+            <li><a href="index.html">Contact</a></li>
+          </ul>
+        </div>
+    `
+  } else{
+    currentBurgerState = false;
+    document.querySelector(".js-burger-menu").innerHTML = `
+      <div class="burger-menu">
+          <ul class="burger-links" >
+            <li><a href="index.html">Projects</a></li>
+            <li><a href="index.html">About</a></li>
+            <li><a href="index.html">Blog</a></li>
+            <li><a href="index.html">Contact</a></li>
+          </ul>
+        </div>
+    `
+  }
+}
+
+function loadPage() {
+  insertHeader()
+}
+
+loadPage();
+
