1ba0b2091dd6c9016b6525b296eb1d2e6109172d
Ryan Vemulapalli <ryanvemulapalli@gmail.com>
2025-03-15 22:22:36 -0400

    Made the footer breakout menu interactive and each button has its own content using the DOM

--- src/javascript/footer.js
+++ src/javascript/footer.js
@@ -4,5 +4,39 @@ function insertFooter() {
     .then(data => document.getElementById('footer').innerHTML = data);
 }
 
+let data = '';
+function insertBreakMenu(buttonId) {
+
+  if (buttonId === 'project'){
+    data = `<h1>Hello World1</h1>`
+  } else if (buttonId === 'about') {
+    data = `<h1>Hello World2</h1>`
+  } else if (buttonId === 'blog') {
+    data = `<h1>Hello World3</h1>`
+  } else if (buttonId === 'contact') {
+    data = `<h1>Hello World4</h1>`
+  } else { 
+    console.log("Failed, Incorrect buttonID!")
+  }
+
+  document.getElementById('breakout').innerHTML = data
+}
+
+// Toggles the visibility of the burger menu
+let currentBreakOutState = false;
+function toggleBreakOutMenu(buttonId){
+  const burgerMenu = document.querySelector(".breakout-menu"); 
+
+  if (currentBreakOutState) {
+    burgerMenu.classList.remove("active");
+  } else {
+    burgerMenu.classList.add("active");
+  }
+
+  currentBreakOutState = !currentBreakOutState;
+  
+  insertBreakMenu(buttonId);
+}
+
 // Loads the footer on the page
 insertFooter()
