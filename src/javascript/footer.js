7ee74d79d5c9e737e97b0f76a5b7363ed5ca9ecd
Ryan Vemulapalli <ryanvemulapalli@gmail.com>
2025-03-18 20:26:57 -0400

    Footer adjusts based on width, elements also style depending on width. First version of the footer

--- src/javascript/footer.js
+++ src/javascript/footer.js
@@ -26,17 +26,63 @@ function insertBreakMenu(buttonId) {
 let currentBreakOutState = false;
 function toggleBreakOutMenu(buttonId){
   const burgerMenu = document.querySelector(".breakout-menu"); 
+  const buttonRotate = document.getElementById(buttonId);
+  const dropIcon = document.querySelectorAll(".drop-icon");  
 
   if (currentBreakOutState) {
     burgerMenu.classList.remove("active");
+    dropIcon.forEach((icon) => {
+      icon.classList.remove("rotate-img");
+    });
   } else {
     burgerMenu.classList.add("active");
+    buttonRotate.classList.add("rotate-img");
   }
+
+  oldButtonId = buttonId;
 
   currentBreakOutState = !currentBreakOutState;
   
   insertBreakMenu(buttonId);
 }
+
+// Removes the burger menu when the screen is > 975px
+
+window.addEventListener("resize", () => {
+  const footerRight = document.querySelector(".footer-right");
+  const footerLink = document.querySelector(".footer-icon-link");
+  const socialIcon = document.querySelector(".social-icon");
+  const socialRow = document.querySelectorAll(".social-row");
+
+  if (window.innerWidth > 700) {
+    if (footerRight) {
+      footerRight.classList.add("large");
+    }
+    
+    if (footerLink) {
+      footerLink.classList.remove("small");
+    }
+
+    if (socialIcon) {
+      socialIcon.classList.remove("small");
+    }
+
+    if(socialRow){
+      socialRow.forEach((row) => {
+          row.classList.remove("small");
+      });
+    }
+
+  } else {
+    footerRight.classList.remove("large");
+    footerLink.classList.add("small");
+    socialIcon.classList.add("small");
+    socialRow.forEach((row) => {
+      row.classList.add("small");
+    });
+  }
+
+});
 
 // Loads the footer on the page
 insertFooter()
